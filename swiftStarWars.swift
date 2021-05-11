df138a6615eb901b0439cd440574819200ecc894
Rohan arya <82590336+rohanarya165@users.noreply.github.com>
2021-05-12 00:59:36 +0530

    Add files via upload

---
+++ swiftStarWars.swift
@@ -0,0 +1,15 @@
+var episode = 8
+var villain: String 
+
+// Write your code below 🦹
+switch episode{
+  case 1...3:
+  villain = "Emperor Palpatine"
+  case 4...6:
+  villain = "Darth Vader"
+  case 7...9:
+  villain = "Kylo Ren"
+  default:
+  villain = ""
+}
+print(villain)
