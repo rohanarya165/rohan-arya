411be7449fd2cab9fae3cb47a8e5f1a074cba4c1
Rohan arya <82590336+rohanarya165@users.noreply.github.com>
2021-05-12 01:47:28 +0530

    Add files via upload

---
+++ swiftSecret.swift
@@ -0,0 +1,15 @@
+var alphabet:[Character] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var secretMessage = "codecademy".lowercased()
+var message = Array(secretMessage)
+print(message)
+print(secretMessage)
+for i in 0 ..< message.count {
+ for j in 0 ..< alphabet.count {
+   if message[i] == alphabet[j]{
+     message[i] = alphabet[(j+3)%26]
+     break
+   }
+ }
+ 
+}
+print(message)
